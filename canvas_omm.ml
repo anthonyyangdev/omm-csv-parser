0fa626be532da616ebd4a76a83b8abe1a1d7567f
Anthony Yang <ay339@cornell.edu>
2020-09-14 03:34:48 -0400

    Update

--- canvas_omm.ml
+++ canvas_omm.ml
@@ -1,10 +1,10 @@
-(** [section_index header[] is possibly the index of the section value in the
+(** [section_index header] is possibly the index of the section value in the
     Csv downloaded from Canvas's OMM module.
 *)
 let section_index header =
   let rec helper index = function
     | [] -> None
-    | h::t -> 
+    | h::t ->
       if String.trim h = "section" then Some index
       else helper (index + 1) t
   in helper 0 header
@@ -15,7 +15,7 @@ let section_index header =
     for each student in [students].
 *)
 let filter_csv students section_index section =
-  (** [filter_fun student] returns [true] if index [section_index] of
+  (* [filter_fun student] returns [true] if index [section_index] of
       [student] has the substring ["DIS" ^ string_of_int section].
   *)
   let filter_fun student =
@@ -42,11 +42,28 @@ let filter filename section =
     | None -> raise (Invalid_argument("Csv does not contain sections."))
     | Some i -> header::(filter_csv students i section)
 
+module StringMap = Map.Make(String)
+
+(** [options map] is the list of CLI options and specifications for
+    [Arg.parse].
+*)
+let options map: (string * Arg.spec * string) list =
+  let add_input = fun i -> map := StringMap.add "i" i !map in
+  let add_section = fun s -> map := StringMap.add "s" s !map in
+  let add_output = fun o -> map := StringMap.add "o" o !map in
+  [
+    ("-i", Arg.String add_input, "The input csv from Canvas");
+    ("-s", Arg.String add_section, "The section number");
+    ("-o", Arg.String add_output, "The output file")
+  ]
+
 (** [main ()] begins the program. *)
 let main () =
-  let filename = Sys.argv.(1) in
-  let section = Sys.argv.(2) |> int_of_string in
-  let output = Sys.argv.(3) in
+  let map = ref (StringMap.singleton "o" "omm.csv") in
+  Arg.parse (options map) (fun i -> map := StringMap.add "i" i !map) "";
+  let filename = StringMap.find "i" !map in
+  let section = StringMap.find "s" !map |> int_of_string in
+  let output = StringMap.find "o" !map in
   let filtered = filter filename section in
   Csv.save output filtered
 
